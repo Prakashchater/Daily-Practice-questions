f60b3044c36e4fa14b140614e302ede16be778fd
Prakash <prakashchater@gmail.com>
2021-05-09 23:47:31 +0530

    09/May2021

--- Arrays/SumofInfiniteArr.py
+++ Arrays/SumofInfiniteArr.py
@@ -13,12 +13,6 @@ def func(sumArray, x, n):
     return res
 
 
-'''
-    Time Complexity:O(Q*(R-L))
-    Space Complexity:O(1).
-    Where Q is the number of given queries, and L and R are the give
-'''
-
 
 def sumInRanges(arr, n, queries, q):
     mod = 10 ** 9 + 7
@@ -26,51 +20,55 @@ def sumInRanges(arr, n, queries, q):
     #  It stores answer for each query.
     ans = []
 
-    # Traversing the given queries
+    # It store cumulative sum where sumArray[i] = sum(A[0]+..A[i]).
+    sumArray = [0 for i in range(n + 1)]
+
+    for i in range(1, n + 1):
+        sumArray[i] = (sumArray[i - 1] + arr[i - 1]) % mod
+
+    # Traversing the given queries.
     for ranges in queries:
-        # Substract 1 from both L and R to use it as 0-based indexing
-        l = ranges[0] - 1
-        r = ranges[1] - 1
 
-        # It stores the sum
-        sume = 0
+        l = ranges[0]
+        r = ranges[1]
 
-        for i in range(l, r + 1):
-            index = (i % n)
-            sume = (sume + arr[index]) % mod
+        #  It stores the prefix sum from index 0 to index r in an infinite array.
+        rsum = func(sumArray, r, n)
 
-        sume %= mod
-        # Add answer to each query
-        ans.append(sume)
+        # It stores the prefix sum from index 0 to index l-1 in an infinite array.
+        lsum = func(sumArray, l - 1, n)
+
+        # Add answer for each query.
+        ans.append((rsum - lsum + mod) % mod)
 
     return ans
 
-
+'''
+    Time Complexity:O(Q*(R-L))
+    Space Complexity:O(1).
+    Where Q is the number of given queries, and L and R are the give
+'''
 # def sumInRanges(arr, n, queries, q):
 #     mod = 10 ** 9 + 7
 #
 #     #  It stores answer for each query.
 #     ans = []
 #
-#     # It store cumulative sum where sumArray[i] = sum(A[0]+..A[i]).
-#     sumArray = [0 for i in range(n + 1)]
-#
-#     for i in range(1, n + 1):
-#         sumArray[i] = (sumArray[i - 1] + arr[i - 1]) % mod
-#
-#     # Traversing the given queries.
+#     # Traversing the given queries
 #     for ranges in queries:
+#         # Substract 1 from both L and R to use it as 0-based indexing
+#         l = ranges[0] - 1
+#         r = ranges[1] - 1
 #
-#         l = ranges[0]
-#         r = ranges[1]
+#         # It stores the sum
+#         sume = 0
 #
-#         #  It stores the prefix sum from index 0 to index r in an infinite array.
-#         rsum = func(sumArray, r, n)
+#         for i in range(l, r + 1):
+#             index = (i % n)
+#             sume = (sume + arr[index]) % mod
 #
-#         # It stores the prefix sum from index 0 to index l-1 in an infinite array.
-#         lsum = func(sumArray, l - 1, n)
-#
-#         # Add answer for each query.
-#         ans.append((rsum - lsum + mod) % mod)
+#         sume %= mod
+#         # Add answer to each query
+#         ans.append(sume)
 #
 #     return ans
